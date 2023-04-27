f585b59ddb7ae4c84f93bf931a948c7cb3f1e058
Jini Byun <jinibyun@gmail.com>
2023-04-27 15:02:41 -0400

    transaction and exception handling

---
+++ SQL/sources/02dev1/44_transaction_more.sql
@@ -0,0 +1,137 @@
+/*********************************
+transaction
+*********************************/
+-- 가장 기본적인 부분에 대해서는 "26_modifying_transaction.sql" 에서 진행을 했고, 여기에서는 좀 더 나가서 "Explicit Transaction" 을 기준으로 깊이 있게 설명하기로 한다.
+
+/*------- Transaction Types -------*/
+-- 1. Auto Commit Transaction (default) : 자동으로 Error 를 발생해서, 데이터 변경을 취소. 하지만, 두 가지 이상의 DML 에 대해서 모든 변경을 실행/취소 할 수 있는 것은 아니다.
+-- 2. Implicit Transaction : SET IMPLICIT_TRANSACTIONS ON/OFF 를 이용해서 암묵적인 transaction 을 시작할 수 있다.
+-- 3. Explicit Transaction : 주로 이 방법을 통해 진행
+
+-----------------------
+-- Explicit Transaction
+-----------------------
+-- prep
+CREATE SCHEMA test;
+
+CREATE TABLE test.Customer
+(
+    CustomerID INT PRIMARY KEY,
+    CustomerCode VARCHAR(10),
+    CustomerName VARCHAR(50)
+)
+
+-- explicit transaction using sp
+CREATE PROC SPAddCustommer
+@customerId int,
+@customerCode varchar(10),
+@customerName varchar(50)
+AS
+BEGIN
+   BEGIN TRANSACTION
+      INSERT INTO test.Customer VALUES(@customerId, @customerCode, @customerName)
+      IF(@@ERROR > 0) -- @@Error : 전역 변수
+      BEGIN
+         ROLLBACK TRANSACTION
+      END
+      ELSE
+      BEGIN
+         COMMIT TRANSACTION
+      END  
+END
+
+-- test
+exec SPAddCustommer 1, 'CODE_3', 'Pam';
+exec SPAddCustommer 2, 'CODE_4', 'Sara';
+
+-- confirm
+select * from test.Customer
+
+
+-----------------------
+-- Nested Transaction
+-----------------------
+/* 
+기본 구조 
+BEGIN TRANSACTION T1
+      DML 구문.....
+      BEGIN TRANSACTION T2
+            DML 구문..... 
+            PRINT @@TRANCOUNT  -- @@TranCount 전역변수: 2  아직 commit/rollback 되지 않은 open 되어 있는 transcation 숫자 리턴
+      COMMIT TRANSACTION T2 -- NOTE: 아직 완전히 물리적으로 T2 가 실행된 것은 아니다. 제일 바깥에 있는 outer transaction 이 commit 될 때 물리적으로 변경사항이 반영된다. 다른 표현으로 하면 outer transaction 이 모든 변경사항을 "최종" 결정한다.
+
+      PRINT @@TRANCOUNT -- @@TranCount 전역변수: 1
+COMMIT TRANSACTION T1 -- 제일 바깥에 있는 transaction 은 inner transaction commit 사항도 기억하고 함께 물리적으로 DB 에 적용한다.
+PRINT @@TRANCOUNT -- @@TranCount 전역변수: 0
+*/
+
+-- prep
+delete from test.Customer;
+
+-- test: open 되어 있는 transaction 숫자 확인
+BEGIN TRANSACTION T1
+      INSERT INTO test.Customer VALUES (10, 'Code_10', 'Ramesh')
+      INSERT INTO test.Customer VALUES (11, 'Code_11', 'Suresh')
+      BEGIN TRANSACTION T2
+            INSERT INTO test.Customer VALUES (12, 'Code_12', 'Priyanka')
+            INSERT INTO test.Customer VALUES (13, 'Code_13', 'Preety')   
+            PRINT @@TRANCOUNT
+      COMMIT TRANSACTION T2
+      PRINT @@TRANCOUNT
+COMMIT TRANSACTION T1
+PRINT @@TRANCOUNT
+
+
+-------------------------------------------------
+-- Partial Rollback
+-------------------------------------------------
+-- prep
+delete from test.Customer;
+
+
+-- Trasaction 시작시 각 각의 DML 구문의 적당한 자리에 Partial Rollback 이 가능하도록 Save Transaction 을 삽입하여 일종의 Point 정보를 둔다.
+BEGIN TRANSACTION 
+ SAVE TRANSACTION SavePoint1 -- SAVE TRANSACTION 을 통해 전체 ROLLBACK 이 아닌 부분적인 ROLLBACK (Partial Rollback) 을 가능하게 한다.
+     INSERT INTO test.Customer VALUES (1, 'Code_1', 'Ramesh')
+     INSERT INTO test.Customer VALUES (2, 'Code_2', 'Suresh')
+ SAVE TRANSACTION SavePoint2
+     INSERT INTO test.Customer VALUES (3, 'Code_3', 'Priyanka')
+     INSERT INTO test.Customer VALUES (4, 'Code_4', 'Preety')
+ SAVE TRANSACTION SavePoint3
+     INSERT INTO test.Customer VALUES (5, 'Code_5', 'John')
+     INSERT INTO test.Customer VALUES (6, 'Code_6', 'David')
+
+-- 이 후에 
+Rollback Transaction SavePoint2; -- SavePoint2 를 찾아 그 시점부터 시작되는 모든 DML 은 rollback 한다는 의미
+
+-- 마지막으로
+Commit Transaction; 
+
+-- test: 결과 확인
+select * from test.Customer
+
+
+-------------------------------------------------
+-- Nested Transaction with SavePoint
+-------------------------------------------------
+-- prep
+delete from test.Customer;
+
+BEGIN TRANSACTION T1
+    SAVE TRANSACTION SavePoint1
+         INSERT INTO test.Customer VALUES (10, 'Code_10', 'Ramesh')
+         INSERT INTO test.Customer VALUES (11, 'Code_11', 'Suresh')
+ 
+     BEGIN TRANSACTION T2
+          SAVE TRANSACTION SavePoint2
+               INSERT INTO test.Customer VALUES (12, 'Code_12', 'Priyanka')
+               INSERT INTO test.Customer VALUES (13, 'Code_13', 'Preety')   
+
+     COMMIT TRANSACTION T2 
+
+     ROLLBACK TRANSACTION SavePoint2 -- 마지막 commit 이 이뤄지기 직전 SavePoint2 를 찾아서 그 시점 부터 이뤄졌던 transaction 을 취소
+
+COMMIT TRANSACTION T1 -- 앞서 설명 했듯이, 맨 마지막 COMMIT TRANSCTION 이 실행되어야 비로소 inner transaction 이 실행되는 것이다.
+
+-- test
+select * from test.Customer;
---
+++ SQL/sources/02dev1/45_exception_handling.sql
@@ -0,0 +1,287 @@
+/*********************************
+exception handling
+*********************************/
+/* ------- error hanling 이 없는 sp ------ */
+CREATE PROCEDURE spDivideTwoNumber (
+    @Number1 INT
+    , @Number2 INT
+    )
+AS
+BEGIN
+    DECLARE @Result INT
+
+    SET @Result = 0
+    SET @Result = @Number1 / @Number2
+
+    PRINT 'RESULT IS :' + CAST(@Result AS VARCHAR)
+END
+
+-- test
+EXEC spDivideTwoNumber 100
+    , 0 -- 결과를 보면 예상되는 대로 error 가 발생되지만, 적절히 error 에 대해 handling 하지는 않고 있다.
+
+-- NOTE: 다른 프로그래밍 언어와는 다르게, SQL 에서 error 발생해도 그 다음 구문이 계속 실행된다는 점이다. (결과 RESULT 가 보여지고 있다)
+/* ------- error hanling 적용 ------ */
+-------------------------------------------------
+-- RaiseError()
+-------------------------------------------------
+-- 다음과 같이 error handling 코드를 삽입하여 수정 
+ALTER PROCEDURE spDivideTwoNumber @Number1 INT
+    , @Number2 INT
+AS
+BEGIN
+    DECLARE @Result INT
+
+    SET @Result = 0
+
+    IF (@Number2 = 0)
+    BEGIN
+        RAISERROR (
+                'Second Number Cannot be zero'
+                , 16
+                , 1
+                ) -- system error message 를 리턴하는 함수
+            -- RAISERROR('Error Message', ErrorSeverity, ErrorState)
+            -- Error Severity 는 16 번 Error State 는 1 번으로 setting 하는 이유
+            -- Error Severity: When we are returning any custom errors in SQL Server, we need to set the ErrorSeverity level as 16, which indicates this is a general error and this error can be corrected by the user. In our example, the error can be corrected by the user by giving a nonzero value for the second parameter
+            -- Error State: The ErrorState is also an integer value between 1 and 255. The RAISERROR() function can only generate custom errors if you set the Error State value between 1 to 127.
+    END
+    ELSE
+    BEGIN
+        SET @Result = @Number1 / @Number2
+
+        PRINT 'RESULT IS : ' + CAST(@Result AS VARCHAR)
+    END
+END
+
+-- test
+EXEC spDivideTwoNumber 100
+    , 0
+
+-------------------------------------------------
+-- @@error  전역 변수 이용하기
+-------------------------------------------------
+ALTER PROCEDURE spDivideTwoNumber @Number1 INT
+    , @Number2 INT
+AS
+BEGIN
+    DECLARE @Result INT
+
+    SET @Result = 0
+
+    IF (@Number2 = 0)
+    BEGIN
+        RAISERROR (
+                'Second Number Cannot be zero'
+                , 16
+                , 1
+                )
+    END
+    ELSE
+    BEGIN
+        SET @Result = @Number1 / @Number2
+    END
+
+    IF (@@ERROR <> 0) -- 추가적으로 Error 발생 상황을 핸들링
+    BEGIN
+        PRINT 'Error Occurred'
+    END
+    ELSE
+    BEGIN
+        PRINT 'RESULT IS :' + CAST(@Result AS VARCHAR)
+    END
+END
+
+-- test
+EXEC spDivideTwoNumber 100
+    , 0
+
+-----------------------------------------------------------
+-- RaiseError, @@error 그리고 transaction 을 이용하여 sp 생성
+------------------------------------------------------------
+-- prep
+CREATE TABLE test.Product (
+    ProductId INT PRIMARY KEY
+    , Name VARCHAR(50)
+    , Price INT
+    , QuantityAvailable INT
+    )
+GO
+
+-- Populate the Product Table with some test data
+INSERT INTO test.Product
+VALUES (
+    101
+    , 'Laptop'
+    , 1234
+    , 100
+    )
+
+INSERT INTO test.Product
+VALUES (
+    102
+    , 'Desktop'
+    , 3456
+    , 50
+    )
+
+INSERT INTO test.Product
+VALUES (
+    103
+    , 'Tablet'
+    , 5678
+    , 35
+    )
+
+INSERT INTO test.Product
+VALUES (
+    104
+    , 'Mobile'
+    , 7890
+    , 25
+    )
+GO
+
+CREATE TABLE test.ProductSales (
+    ProductSalesId INT PRIMARY KEY
+    , ProductId INT
+    , QuantitySold INT
+    )
+GO
+
+INSERT INTO test.ProductSales
+VALUES (
+    1
+    , 101
+    , 5
+    )
+
+INSERT INTO test.ProductSales
+VALUES (
+    2
+    , 102
+    , 7
+    )
+
+INSERT INTO test.ProductSales
+VALUES (
+    3
+    , 103
+    , 5
+    )
+
+INSERT INTO test.ProductSales
+VALUES (
+    4
+    , 104
+    , 7
+    )
+GO
+
+-- sp 생성
+CREATE PROCEDURE spSellProduct @ProductID INT
+    , @QuantityToSell INT
+AS
+BEGIN
+    -- 팔려고 하는 상품의 존재 여부 체크
+    -- TODO
+
+
+    -- 팔려고 하는 상품의 stock 체크
+    DECLARE @StockAvailable INT
+
+    SELECT @StockAvailable = QuantityAvailable
+    FROM test.Product
+    WHERE ProductId = @ProductId
+
+    -- 상품 체크 validation
+    IF (@StockAvailable < @QuantityToSell)
+    BEGIN
+        -- 참고로 THROW 구문을 사용할 수도 있다.
+        RAISERROR (
+                'Enough Stock is not available'
+                , 16
+                , 1
+                )
+    END
+    ELSE
+    BEGIN
+        BEGIN TRANSACTION
+
+        -- 재고량 update
+        UPDATE test.Product
+        SET QuantityAvailable = (QuantityAvailable - @QuantityToSell)
+        WHERE ProductID = @ProductID
+
+        -- 별도로 Identity 값으로 지정된 것이 아니기 때문에 다음과 같이 Max 값을 얻어야 함
+        DECLARE @MaxProductSalesId INT
+
+        SELECT @MaxProductSalesId = CASE 
+                WHEN MAX(ProductSalesId) IS NULL
+                    THEN 0
+                ELSE MAX(ProductSalesId)
+                END
+        FROM test.ProductSales
+
+        SET @MaxProductSalesId = @MaxProductSalesId + 1
+
+        -- ProductSales table 에 값을 입력
+        INSERT INTO test.ProductSales (
+            ProductSalesId
+            , ProductId
+            , QuantitySold
+            )
+        VALUES (
+            @MaxProductSalesId
+            , @ProductId
+            , @QuantityToSell
+            )
+
+        -- transaction 의 commit 혹은 rollback 여부를 결정
+        IF (@@ERROR <> 0)
+        BEGIN
+            ROLLBACK TRANSACTION
+            PRINT 'Rolled Back the Transaction'
+        END
+        ELSE
+        BEGIN
+            COMMIT TRANSACTION
+            PRINT 'Committed the Transaction'
+        END
+    END
+END
+
+-- test
+exec spSellProduct 101, 500 -- 에러 발생
+
+------------------------------
+-- Try...Catch... 구문 이용하기
+------------------------------
+CREATE PROCEDURE spDivideTwoNumbers @Number1 INT
+    , @Number2 INT
+AS
+BEGIN
+    DECLARE @Result INT
+
+    SET @Result = 0
+
+    BEGIN TRY
+        SET @Result = @Number1 / @Number2
+        PRINT 'RESULT IS : ' + CAST(@Result AS VARCHAR)
+    END TRY
+
+    BEGIN CATCH
+        -- PRINT 'SECOND NUMBER SHOULD NOT BE ZERO'
+        -- error 관련 시스템 함수들: 이 중에서 Error_message 와 Error_Procedure 는 많이 사용되는 함수들
+        SELECT  ERROR_NUMBER() as ErrorNumber,
+                ERROR_MESSAGE() as ErrorMessage,
+                ERROR_PROCEDURE() as ErrorProcedure,
+                ERROR_STATE() as ErrorState,
+                ERROR_SEVERITY() as ErrorSeverity,
+                ERROR_LINE() as ErrorLine
+    END CATCH
+END
+
+-- test
+exec spDivideTwoNumbers 10, 0
+
